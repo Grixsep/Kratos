3fa995087db03bc275d620bf7b2920a607453673
Lluis de Miguel <lmiguel@cimne.upc.edu>
2025-05-08 19:50:32 +0200

    Formatting

--- applications/LaserDrillingApplication/test_examples/axisym_ablation_plus_thermal_problem/2.1.1_json_creator.py
+++ applications/LaserDrillingApplication/test_examples/axisym_ablation_plus_thermal_problem/2.1.1_json_creator.py
@@ -2,14 +2,14 @@ import json
 import os
 
 # List of beam_waist_diameter values
-beam_waist_diameters = [0.02, 0.025, 0.03]
+beam_waist_diameters = [0.02, 0.07, 0.12]
 
 # Load the input JSON file
-with open('ProjectParameters.json', 'r') as file:
+with open("ProjectParameters.json", "r") as file:
     data = json.load(file)
 
 # Create output directory if it doesn't exist
-output_dir = 'parameter_jsons'
+output_dir = "parameter_jsons"
 os.makedirs(output_dir, exist_ok=True)
 
 # Generate a new JSON file for each beam_waist_diameter value
@@ -18,14 +18,18 @@ for i, diameter in enumerate(beam_waist_diameters):
     modified_data = json.loads(json.dumps(data))
 
     # Modify the beam_waist_diameter value
-    modified_data['problem_data']['beam_waist_diameter'] = diameter
+    modified_data["problem_data"]["beam_waist_diameter"] = diameter
 
     # Modify the output_name in gid_output
-    modified_data['output_processes']['gid_output'][0]['Parameters']['output_name'] = f"gid_output/waist_{i}/axisym_ablation_plus_thermal_problem"
+    modified_data["output_processes"]["gid_output"][0]["Parameters"]["output_name"] = (
+        f"gid_output/waist_{i}/axisym_ablation_plus_thermal_problem"
+    )
 
     # Write the modified data to a new JSON file
-    output_filename = os.path.join(output_dir, f'ProjectParameters_{i}.json')
-    with open(output_filename, 'w') as file:
+    output_filename = os.path.join(output_dir, f"ProjectParameters_{i}.json")
+    with open(output_filename, "w") as file:
         json.dump(modified_data, file, indent=4)
 
-    print(f'Created {output_filename} with beam_waist_diameter = {diameter} and output_name = gid_output/waist_{i}/axisym_ablation_plus_thermal_problem')
+    print(
+        f"Created {output_filename} with beam_waist_diameter = {diameter} and output_name = gid_output/waist_{i}/axisym_ablation_plus_thermal_problem"
+    )
--- applications/LaserDrillingApplication/test_examples/axisym_ablation_plus_thermal_problem/2.1.2_batch_running.py
+++ applications/LaserDrillingApplication/test_examples/axisym_ablation_plus_thermal_problem/2.1.2_batch_running.py
@@ -24,15 +24,18 @@ if __name__ == "__main__":
     else:  # Load all JSON files from parameter_jsons directory
         if not os.path.exists(parameter_dir):
             raise Exception(f"Directory '{parameter_dir}' does not exist!")
-        file_list = [os.path.join(parameter_dir, f) for f in os.listdir(parameter_dir)
-                    if f.endswith('.json') and os.path.isfile(os.path.join(parameter_dir, f))]
+        file_list = [
+            os.path.join(parameter_dir, f)
+            for f in os.listdir(parameter_dir)
+            if f.endswith(".json") and os.path.isfile(os.path.join(parameter_dir, f))
+        ]
 
     if not file_list:
         raise Exception(f"No JSON files found in directory '{parameter_dir}'!")
 
     # Run simulation for each JSON file
     for project_parameters_file_name in file_list:
-        print(f"Running simulation with parameters from: {project_parameters_file_name}")
+        print(f"------------ Running simulation with parameters from: {project_parameters_file_name} -------------")
 
         with open(project_parameters_file_name, "r") as parameter_file:
             parameters = KratosMultiphysics.Parameters(parameter_file.read())
@@ -41,4 +44,4 @@ if __name__ == "__main__":
         simulation = LaserDrillingAnalysis(model, parameters)
         simulation.Run()
 
-        print(f"Completed simulation for: {project_parameters_file_name}")
+        print(f"------------ Completed simulation for: {project_parameters_file_name} -------------")
