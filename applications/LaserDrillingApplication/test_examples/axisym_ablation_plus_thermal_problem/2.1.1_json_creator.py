d1876be3a66b5311dd843ba5dc831d5dc209bc3b
Lluis de Miguel <lmiguel@cimne.upc.edu>
2025-05-12 01:04:06 +0200

    Create a directory for the batch run with subdirectories for each parameter configuration

--- applications/LaserDrillingApplication/test_examples/axisym_ablation_plus_thermal_problem/2.1.1_json_creator.py
+++ applications/LaserDrillingApplication/test_examples/axisym_ablation_plus_thermal_problem/2.1.1_json_creator.py
@@ -1,35 +1,93 @@
 import json
 import os
+import sys
 
 # List of beam_waist_diameter values
-beam_waist_diameters = [0.02, 0.07, 0.12]
+beam_waist_diameters = [0.02, 0.071, 0.12]
 
-# Load the input JSON file
-with open("ProjectParameters.json", "r") as file:
-    data = json.load(file)
+# Load the input JSON files
+project_parameters_input_filename = os.path.join("parameters", "ProjectParameters.json")
+laser_parameters_input_filename = os.path.join("parameters", "LaserParameters.json")
+materials_parameters_input_filename = os.path.join("parameters", "MaterialsParameters.json")
+environment_parameters_input_filename = os.path.join("parameters", "EnvironmentParameters.json")
+
+with open(project_parameters_input_filename, "r") as input_project_file:
+    project_data = json.load(input_project_file)
+# Create a deep copy of the original data
+modified_project_data = json.loads(json.dumps(project_data))
+
+with open(laser_parameters_input_filename, "r") as input_laser_file:
+    laser_data = json.load(input_laser_file)
+# Create a deep copy of the original data
+modified_laser_data = json.loads(json.dumps(laser_data))
+
+with open(materials_parameters_input_filename, "r") as input_materials_file:
+    materials_data = json.load(input_materials_file)
+# Create a deep copy of the original data
+modified_materials_data = json.loads(json.dumps(materials_data))
+
+with open(environment_parameters_input_filename, "r") as input_environment_file:
+    environment_data = json.load(input_environment_file)
+# Create a deep copy of the original data
+modified_environment_data = json.loads(json.dumps(environment_data))
+
 
 # Create output directory if it doesn't exist
-output_dir = "parameter_jsons"
-os.makedirs(output_dir, exist_ok=True)
+batches_output_dir = "waist_varying_batch"
+try:
+    os.makedirs(batches_output_dir, exist_ok=False)
+except FileExistsError:
+    print("The directory for the batch run already exists")
+    sys.exit(1)
 
-# Generate a new JSON file for each beam_waist_diameter value
-for i, diameter in enumerate(beam_waist_diameters):
-    # Create a deep copy of the original data
-    modified_data = json.loads(json.dumps(data))
 
-    # Modify the beam_waist_diameter value
-    modified_data["problem_data"]["beam_waist_diameter"] = diameter
+# Generate new parameters files for each beam_waist_diameter value
+for i, beam_waist_diam in enumerate(beam_waist_diameters):
+    # Create an output directory for each parameter configuration
+    batch_dir = os.path.join(batches_output_dir, f"beam_waist_{i}")
+    os.makedirs(batch_dir, exist_ok=True)
 
+    # Modify ProjectParameters
     # Modify the output_name in gid_output
-    modified_data["output_processes"]["gid_output"][0]["Parameters"]["output_name"] = (
-        f"gid_output/waist_{i}/axisym_ablation_plus_thermal_problem"
+    modified_project_data["output_processes"]["gid_output"][0]["Parameters"]["output_name"] = os.path.join(
+        batch_dir, "gid_output", "axisym_ablation_plus_thermal_problem"
     )
 
-    # Write the modified data to a new JSON file
-    output_filename = os.path.join(output_dir, f"ProjectParameters_{i}.json")
-    with open(output_filename, "w") as file:
-        json.dump(modified_data, file, indent=4)
-
-    print(
-        f"Created {output_filename} with beam_waist_diameter = {diameter} and output_name = gid_output/waist_{i}/axisym_ablation_plus_thermal_problem"
+    # Modify the path of the parameter files
+    modified_project_data["solver_settings"]["material_import_settings"]["materials_filename"] = os.path.join(
+        batch_dir, "MaterialsParameters.json"
     )
+    modified_project_data["solver_settings"]["material_import_settings"]["laser_filename"] = os.path.join(
+        batch_dir, "LaserParameters.json"
+    )
+    modified_project_data["solver_settings"]["material_import_settings"]["environment_filename"] = os.path.join(
+        batch_dir, "EnvironmentParameters.json"
+    )
+
+    # Output the new ProjectParameters
+    output_project_filename = os.path.join(batch_dir, "ProjectParameters.json")
+    with open(output_project_filename, "w") as output_project_file:
+        json.dump(modified_project_data, output_project_file, indent=4)
+
+    # Modify LaserParameters
+    # Modify the beam_waist_diameter value
+    modified_laser_data["properties"][0]["Material"]["Variables"]["beam_waist_diameter"] = beam_waist_diam
+
+    # Output the new LaserParameters
+    output_laser_filename = os.path.join(batch_dir, "LaserParameters.json")
+    with open(output_laser_filename, "w") as output_laser_file:
+        json.dump(modified_laser_data, output_laser_file, indent=4)
+
+    # Modify MaterialsParameters
+    # Do nothing in this case
+    # Output the new MaterialsParameters
+    output_materials_filename = os.path.join(batch_dir, "MaterialsParameters.json")
+    with open(output_materials_filename, "w") as output_materials_file:
+        json.dump(modified_materials_data, output_materials_file, indent=4)
+
+    # Modify EnvironmentParameters
+    # Do nothing in this case
+    # Output the new EnvironmentParameters
+    output_environment_filename = os.path.join(batch_dir, "EnvironmentParameters.json")
+    with open(output_environment_filename, "w") as output_environment_file:
+        json.dump(modified_environment_data, output_environment_file, indent=4)
